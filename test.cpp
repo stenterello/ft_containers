0b23062462cf5b6a25401fdc097334bb707babd4
stenterello <davide.delladio@gmail.com>
2022-11-21 13:16:24 +0100

    testing set constructor

--- test.cpp
+++ test.cpp
@@ -1,39 +1,107 @@
-#include "iterator.hpp"
-#include "rb_tree.hpp"
 #include <iostream>
+#include <vector>
+#include <ctime>
+#include <iomanip>
+#include <sys/time.h>
+#include <unistd.h>
+#include <set>
+#include "set.hpp"
 
-int	main()
-{
-	ft::RBTreeSet<int, ft::Node<int> >				ciao;
-	ft::RBTreeSet<int, ft::Node<int> >::iterator	iter;
+#define _set	ft::set
+const std::string GREEN = "\x1B[1;32m";
+const std::string REDD = "\x1B[1;31m";
+const std::string YELLOW = "\x1B[1;33m";
+const std::string WHITE = "\x1B[1;39m";
+const std::string RESET = "\033[0m";
 
-	ciao.insert(6);
-	ciao.insert(2);
-	ciao.insert(1);
-	ciao.insert(8);
-	ciao.insert(4);
-	ciao.insert(3);
-	ciao.insert(12);
-	ciao.insert(9);
-	ciao.insert(15);
-	ciao.insert(27);
-	ciao.insert(48);
-	ciao.insert(42);
-	ciao.insert(35);
-	ciao.insert(5);
-	ciao.insert(22);
-	ciao.insert(6);
-	ciao.insert(7);
-	iter = ciao.find(3);
-	if (iter.node)
-		std::cout << *iter << std::endl;
-	std::cout << ciao.count(ciao.begin()) << std::endl;
-	std::cout << (ciao.min()->data) << std::endl;
-	std::cout << (ciao.max()->data) << std::endl;
-	std::cout << ciao.getPredecessor(iter.node)->data << std::endl;
-	std::cout << "count before " << ciao.count() << std::endl;
-	ciao.erase(1);
-	std::cout << "count after " << ciao.count() << std::endl;
+volatile static time_t g_start1;
+volatile static time_t g_start2;
+volatile static time_t g_end1;
+volatile static time_t g_end2;
 
+time_t timer() {
+	struct timeval start = {};
+	gettimeofday(&start, nullptr);
+	time_t msecs_time = (start.tv_sec * 1000) + (start.tv_usec / 1000);
+	return msecs_time;
+}
+
+void printElement(std::string t) {
+	if (t == "OK")
+	    t = GREEN + t + RESET;
+	else if (t == "FAILED") t = REDD + t + RESET;
+	std::cout << std::left << std::setw(30) << std::setfill(' ') << t;
+}
+
+
+int _ratio = 10000;
+
+template <class T>
+int run_set_unit_test(std::string test_name, std::vector<int> (func1)(std::set<T>), std::vector<int> (func2)(_set<T>)) {
+    int    result;
+    int    leaks;
+    time_t t1;
+	time_t t2;
+	std::vector<int > res1;
+	std::vector<int > res2;
+	std::set<int> set;
+	_set<int> my_set;
+
+	printElement(test_name);
+	res1 = func1(set);
+	res2 = func2(my_set);
+	if (res1 == res2) {
+	    printElement("OK");
+	    result = 0;
+	}
+	else {
+	    printElement("FAILED");
+	    result = 1;
+	}
+	t1 = g_end1 - g_start1, t2 = g_end2 - g_start2;
+	(t1 >= t2) ? printElement(GREEN + std::to_string(t2) + "ms" + RESET) : printElement(REDD + std::to_string(t2) + "ms" + RESET);
+	(t1 > t2) ? printElement(REDD + std::to_string(t1) + "ms" + RESET) : printElement(GREEN + std::to_string(t1) + "ms" + RESET);
+	// leaks = leaks_test(getpid());
+	leaks = 0;
+	std::cout << std::endl;
+
+	return !(!result && !leaks);
+}
+
+template <class T>
+std::vector<int> copy_constructor_test(std::set<T> st) {
+    std::vector<int> v;
+
+    for (int i = 0; i < 30 * _ratio; ++i) {
+        st.insert(i);
+    }
+    g_start1 = timer();
+    std::set<int> st2(st.begin(), st.end());
+    g_end1 = timer();
+    std::set<int>::iterator it = st2.begin();
+    for (int i = 0; i < 30 * _ratio; ++i, it++) {
+        v.push_back(*it);
+    }
+    return v;
+}
+
+template <class T>
+std::vector<int> copy_constructor_test(_set<T> st) {
+    std::vector<int> v;
+
+    for (int i = 0; i < 30 * _ratio; ++i)
+        st.insert(i);
+    g_start2 = timer();
+    _set<int> st2(st.begin(), st.end());
+    g_end2 = timer();
+    _set<int>::iterator it = st2.begin();
+    for (int i = 0; i < 30 * _ratio; ++i, it++) {
+        v.push_back(*it);
+    }
+    return v;
+}
+
+int main() {
+    exit(run_set_unit_test<int>("constructor", copy_constructor_test, copy_constructor_test));
 	return (0);
 }
