15ebab9bff557602cea2752fc55bd9393c0faf0c
Davide Delladio <ddelladi@e3r4p1.42roma.it>
2022-12-06 18:13:11 +0100

    foo to fix

--- test.cpp
+++ test.cpp
@@ -1,393 +1,126 @@
-#include <stack>
-#include "stack.hpp"
+
+#define TESTED_TYPE foo<int>
 #include <vector>
-#include <iostream>
-#include <sys/time.h>
-#include <iomanip>
-#include "map.hpp"
-#include <map>
-#include <unistd.h>
+#include "vector.hpp"
 
-#define _stack ft::stack
-#define _vector ft::vector
-#define _map ft::map
-#define _make_pair ft::make_pair
+#define T_SIZE_TYPE typename TESTED_NAMESPACE::vector<T>::size_type
+#define TESTED_NAMESPACE ft
 
-const std::string GREEN = "\x1B[1;32m";
-const std::string REDD = "\x1B[1;31m";
-const std::string YELLOW = "\x1B[1;33m";
-const std::string WHITE = "\x1B[1;39m";
-const std::string RESET = "\033[0m";
+// --- Class foo
+template <typename T>
+class foo {
+	public:
+		typedef T	value_type;
 
-int _ratio = 10000;
-int _allocator_used = 0;
-volatile static time_t g_start1;
-volatile static time_t g_start2;
-volatile static time_t g_end1;
-volatile static time_t g_end2;
+		foo(void) : value(), _verbose(false) { };
+		foo(value_type src, const bool verbose = false) : value(src), _verbose(verbose) { };
+		foo(foo const &src, const bool verbose = false) : value(src.value), _verbose(verbose) { };
+		~foo(void) { if (this->_verbose) std::cout << "~foo::foo()" << std::endl; };
+		void m(void) { std::cout << "foo::m called [" << this->value << "]" << std::endl; };
+		void m(void) const { std::cout << "foo::m const called [" << this->value << "]" << std::endl; };
+		foo &operator=(value_type src) { this->value = src; return *this; };
+		foo &operator=(foo const &src) {
+			if (this->_verbose || src._verbose)
+				std::cout << "foo::operator=(foo) CALLED" << std::endl;
+			this->value = src.value;
+			return *this;
+		};
+		value_type	getValue(void) const { return this->value; };
+		void		switchVerbose(void) { this->_verbose = !(this->_verbose); };
 
-void printElement(std::string t) {
-	if (t == "OK")
-	    t = GREEN + t + RESET;
-	else if (t == "FAILED") t = REDD + t + RESET;
-	std::cout << std::left << std::setw(30) << std::setfill(' ') << t;
-}
-
-time_t timer() {
-	struct timeval start = {};
-	gettimeofday(&start, nullptr);
-	time_t msecs_time = (start.tv_sec * 1000) + (start.tv_usec / 1000);
-	return msecs_time;
-}
-
-std::string exec(const char* cmd) {
-	char buffer[128];
-	std::string result = "";
-	FILE* pipe = popen(cmd, "r");
-	if (!pipe) throw std::runtime_error("popen() failed!");
-	try {
-		while (fgets(buffer, sizeof buffer, pipe) != NULL) {
-			result += buffer;
+		operator value_type(void) const {
+			return value_type(this->value);
 		}
-	} catch (...) { pclose(pipe); throw; }
-	pclose(pipe);
-	return result;
-}
-
-std::string get_leak_string(std::string s) {
-	std::string tmp;
-	int idx = s.find("total leaked bytes");
-	for (; s[idx] != '\n' ; --idx) {}
-	int edx = idx + 1;
-	for (; s[edx] != '\n' ; ++edx) {}
-
-	return s.substr(++idx, edx - 101);
-}
-
-int leaks_test(pid_t pid) {
-	std::string a = "leaks ";
-	a += std::to_string(static_cast<int>(pid));
-	usleep(50);
-	std::string s = get_leak_string(exec(a.c_str()));
-
-	if (s.find("0 leaks for 0 total leaked bytes") != std::string::npos) {
-		printElement(GREEN + "CLEAR" + RESET);
-		return (0);
-	}
-	else {
-	    printElement(REDD + "LEAKS" + RESET);
-	    return (1);
-	}
-}
-
-
-template <class T, class V, class C>
-void fillMap(std::map<T, V, C> &mp) {
-	mp.insert(std::make_pair(16, 3));
-	mp.insert(std::make_pair(8, 3));
-	mp.insert(std::make_pair(23, 3));
-	mp.insert(std::make_pair(7, 3));
-	mp.insert(std::make_pair(19, 3));
-	mp.insert(std::make_pair(29, 3));
-	mp.insert(std::make_pair(41, 3));
-	mp.insert(std::make_pair(4, 3));
-	mp.insert(std::make_pair(11, 3));
-}
-
-template <class T, class V, class C>
-void fillMap(_map<T, V, C> &mp) {
-	mp.insert(_make_pair(16, 3));
-	mp.insert(_make_pair(8, 3));
-	mp.insert(_make_pair(23, 3));
-	mp.insert(_make_pair(7, 3));
-	mp.insert(_make_pair(19, 3));
-	mp.insert(_make_pair(29, 3));
-	mp.insert(_make_pair(41, 3));
-	mp.insert(_make_pair(4, 3));
-	mp.insert(_make_pair(11, 3));
-}
-
-class MapBoolTest {
-public:
-	std::map<int, int> m1, m2, m3, m4;
-	std::map<std::string, std::string> m5, m6, m7, m8, m9;
-	_map<int, int> mp1, mp2, mp3, mp4;
-	_map<std::string, std::string> mp5, mp6, mp7, mp8, mp9;
-
-	MapBoolTest() {
-		m1.insert(std::make_pair(2, 3)); mp1.insert(ft::make_pair(2, 3));
-		m2.insert(std::make_pair(3, 3)); mp2.insert(ft::make_pair(3, 3));
-		m3.insert(std::make_pair(3, 4)); mp3.insert(ft::make_pair(3, 4));
-		m4.insert(std::make_pair(4, 4)); mp4.insert(ft::make_pair(4, 4));
-		m4.insert(std::make_pair(4, 5)); mp4.insert(ft::make_pair(4, 5));
-		m5.insert(std::make_pair("123", "123")); mp5.insert(ft::make_pair("123", "123"));
-		m6.insert(std::make_pair("123", "124")); mp6.insert(ft::make_pair("123", "124"));
-		m7.insert(std::make_pair("124", "123")); mp7.insert(ft::make_pair("124", "123"));
-		m8.insert(std::make_pair("12", "123")); mp8.insert(ft::make_pair("12", "123"));
-		m9.insert(std::make_pair("123", "12")); mp9.insert(ft::make_pair("123", "12"));
-	}
+	private:
+		value_type	value;
+		bool		_verbose;
 };
 
-template <class T, class V>
-int run_map_unit_test(std::string test_name, std::vector<int> (func1)(std::map<T, V>), std::vector<int> (func2)(_map<T, V>)) {
-    int    result;
-    int    leaks;
-	time_t t1;
-	time_t t2;
-	std::vector<int > res1;
-	std::vector<int > res2;
-	std::map<int, int> map;
-	_map<int, int> my_map;
+template <typename T>
+std::ostream	&operator<<(std::ostream &o, foo<T> const &bar) {
+	o << bar.getValue();
+	return o;
+}
+// --- End of class foo
 
-	printElement(test_name);
-	res1 = func1(map);
-	res2 = func2(my_map);
-	if (res1 == res2) {
-	    printElement("OK");
-	    result = 0;
-	}
-	else {
-	    printElement("FAILED");
-	    result = 1;
-	}
-	t1 = g_end1 - g_start1, t2 = g_end2 - g_start2;
-	(t1 >= t2) ? printElement(GREEN + std::to_string(t2) + "ms" + RESET) : printElement(REDD + std::to_string(t2) + "ms" + RESET);
-	(t1 > t2) ? printElement(REDD + std::to_string(t1) + "ms" + RESET) : printElement(GREEN + std::to_string(t1) + "ms" + RESET);
-	leaks = leaks_test(getpid());
-	std::cout << std::endl;
-
-	return !(!result && !leaks);
+template <typename T>
+T	inc(T it, int n)
+{
+	while (n-- > 0)
+		++it;
+	return (it);
 }
 
-template <class T, class V, class C, class A>
-int run_map_allocator_unit_test(std::string test_name, void (func)(_map<T, V, C, A>)) {
+template <typename T>
+T	dec(T it, int n)
+{
+	while (n-- > 0)
+		--it;
+	return (it);
+}
 
-    _map<T, V, C, A> my_map;
+template <typename T>
+void	printSize(TESTED_NAMESPACE::vector<T> const &vct, bool print_content = true)
+{
+	const T_SIZE_TYPE size = vct.size();
+	const T_SIZE_TYPE capacity = vct.capacity();
+	const std::string isCapacityOk = (capacity >= size) ? "OK" : "KO";
+	// Cannot limit capacity's max value because it's implementation dependent
 
-    printElement(test_name);
-    func(my_map);
-    if (_allocator_used) {
-        printElement("OK");
-        std::cout << std::endl;
-        return (0);
-    }
-    else {
-        printElement("FAILED");
-        std::cout << std::endl;
-        return (1);
-    }
+	std::cout << "size: " << size << std::endl;
+	std::cout << "capacity: " << isCapacityOk << std::endl;
+	std::cout << "max_size: " << vct.max_size() << std::endl;
+	if (print_content)
+	{
+		typename TESTED_NAMESPACE::vector<T>::const_iterator it = vct.begin();
+		typename TESTED_NAMESPACE::vector<T>::const_iterator ite = vct.end();
+		std::cout << std::endl << "Content is:" << std::endl;
+		for (; it != ite; ++it)
+			std::cout << "- " << *it << std::endl;
+	}
+	std::cout << "###############################################" << std::endl;
 }
 
 
+int		main(void)
+{
+	const int size = 5;
+	TESTED_NAMESPACE::vector<TESTED_TYPE> vct(size);
+	TESTED_NAMESPACE::vector<TESTED_TYPE>::iterator it(vct.begin());
+	TESTED_NAMESPACE::vector<TESTED_TYPE>::const_iterator ite(vct.end());
 
-int run_bool_unit_test(std::string test_name, bool (func1)()) {
-    int ret = 0;
-    time_t t1;
-    time_t t2;
-    bool res;
+	for (int i = 1; it != ite; ++i)
+		*it++ = i;
+	printSize(vct, 1);
 
-	printElement(test_name);
-	res = func1();
-	if (res) {
-	    printElement("OK");
-	    ret = 0;
-	}
-	else {
-	    printElement("FAILED");
-	    ret = 1;
-	}
-	t1 = g_end1 - g_start1, t2 = g_end2 - g_start2;
-	(t1 >= t2) ? printElement(GREEN + std::to_string(t2) + "ms" + RESET) : printElement(REDD + std::to_string(t2) + "ms" + RESET);
-	(t1 > t2) ? printElement(REDD + std::to_string(t1) + "ms" + RESET) : printElement(GREEN + std::to_string(t1) + "ms" + RESET);
-	std::cout << std::endl;
+	it = vct.begin();
+	ite = vct.begin();
 
-    return ret;
-}
+	std::cout << *(++ite) << std::endl;
+	std::cout << *(ite++) << std::endl;
+	std::cout << *ite++ << std::endl;
+	std::cout << *++ite << std::endl;
 
-template <class T>
-int run_stack_unit_test(std::string test_name, std::vector<int> (func1)(std::stack<T>), std::vector<int> (func2)(_stack<T>)) {
-    int    result;
-    int    leaks;
-    time_t t1;
-	time_t t2;
-	std::vector<int > res1;
-	std::vector<int > res2;
-	std::stack<int> stack;
-	_stack<int> my_stack;
+	it->m();
+	ite->m();
 
-	printElement(test_name);
-	res1 = func1(stack);
-	res2 = func2(my_stack);
-	if (res1 == res2) {
-	    printElement("OK");
-	    result = 0;
-	}
-	else {
-	    printElement("FAILED");
-	    result = 1;
-	}
-	t1 = g_end1 - g_start1, t2 = g_end2 - g_start2;
-	(t1 >= t2) ? printElement(GREEN + std::to_string(t2) + "ms" + RESET) : printElement(REDD + std::to_string(t2) + "ms" + RESET);
-	(t1 > t2) ? printElement(REDD + std::to_string(t1) + "ms" + RESET) : printElement(GREEN + std::to_string(t1) + "ms" + RESET);
-	// leaks = leaks_test(getpid());
-	std::cout << std::endl;
+	std::cout << *(++it) << std::endl;
+	std::cout << *(it++) << std::endl;
+	std::cout << *it++ << std::endl;
+	std::cout << *++it << std::endl;
 
-	leaks = 0;
-	return !(!result && !leaks);;
-}
+	std::cout << *(--ite) << std::endl;
+	std::cout << *(ite--) << std::endl;
+	std::cout << *--ite << std::endl;
+	std::cout << *ite-- << std::endl;
 
-template <class T>
-std::vector<int> constructor_test(std::stack<T> stk) {
-	std::vector<int> v;
-	std::deque<int> deque;
-	for (int i = 0; i < 100 * _ratio; ++i)
-		deque.push_back(i);
-	for (int i = 100 * _ratio; i < 200 * _ratio; ++i)
-		stk.push(i);
-	g_start1 = timer();
-	std::stack<int> stack(deque);
-	std::stack<int> stack2(stk);
-	std::stack<int> stack3;
-	stack3 = stack2;
-	g_end1 = timer();
-	while (stack.size() > 0) {
-		v.push_back(stack.top());
-		stack.pop();
-	}
-	while (stack2.size() > 0) {
-		v.push_back(stack2.top());
-		stack2.pop();
-	}
-	return v;
-}
+	(*it).m();
+	(*ite).m();
 
-template <class T>
-std::vector<int> constructor_test(_stack<T> stk) {
-	std::vector<int> v;
-	_vector<int> deque;
-	for (int i = 0; i < 100 * _ratio; ++i)
-		deque.push_back(i);
-	for (int i = 100 * _ratio; i < 200 * _ratio; ++i)
-		stk.push(i);
-	g_start2 = timer();
-	_stack<int> stack(deque);
-	_stack<int> stack2(stk);
-	_stack<int> stack3;
-	stack3 = stack2;
-	g_end2 = timer();
-	while (stack.size() > 0) {
-		v.push_back(stack.top());
-		stack.pop();
-	}
-	while (stack2.size() > 0) {
-		v.push_back(stack2.top());
-		stack2.pop();
-	}
-	return v;
-}
+	std::cout << *(--it) << std::endl;
+	std::cout << *(it--) << std::endl;
+	std::cout << *it-- << std::endl;
+	std::cout << *--it << std::endl;
 
-
-template <class T, class V>
-std::vector<int> comparator_test(std::map<T, V> mp) {
-
-    std::vector<int> v;
-    fillMap(mp);
-    for (typename std::map<T, V>::iterator it = mp.begin(); it != mp.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp.end(); it != mp.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::greater<int> > mp1;
-    fillMap(mp1);
-    v.push_back(mp1.begin()->first);
-    mp1.erase(41);
-    v.push_back(mp1.begin()->first);
-    mp1.erase(29);
-    v.push_back(mp1.begin()->first);
-    std::map<int, int, std::greater<int> > mp2;
-    mp2.insert(std::make_pair(3, 3));
-    v.push_back(mp2.begin()->first);
-    mp2.erase(3);
-    if (mp2.begin() == mp2.end())
-        v.push_back(1);
-    std::map<int, int, std::plus<int> > mp3;
-    fillMap(mp3);
-    for (typename std::map<T, V>::iterator it = mp3.begin(); it != mp3.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp3.end(); it != mp3.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::minus<int> > mp4;
-    fillMap(mp4);
-    for (typename std::map<T, V>::iterator it = mp4.begin(); it != mp4.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp4.end(); it != mp4.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::greater_equal<int> > mp5;
-    fillMap(mp5);
-    for (typename std::map<T, V>::iterator it = mp5.begin(); it != mp5.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp5.end(); it != mp5.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::multiplies<int> > mp6;
-    fillMap(mp6);
-    for (typename std::map<T, V>::iterator it = mp6.begin(); it != mp6.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp6.end(); it != mp6.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::bit_xor<int> > mp7;
-    fillMap(mp7);
-    for (typename std::map<T, V>::iterator it = mp7.begin(); it != mp7.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp7.end(); it != mp7.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::logical_and<int> > mp8;
-    fillMap(mp8);
-    for (typename std::map<T, V>::iterator it = mp8.begin(); it != mp8.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp8.end(); it != mp8.begin(); it--) { v.push_back(it->first); }
-    g_start1 = timer();
-    g_end1 = timer();
-    v.push_back(mp1.size());
-    return v;
-}
-
-template <class T, class V>
-std::vector<int> comparator_test(_map<T, V> mp) {
-    std::vector<int> v;
-    fillMap(mp);
-    for (typename _map<T, V>::iterator it = mp.begin(); it != mp.end(); it++) { v.push_back(it->first); }
-    for (typename _map<T, V>::iterator it = --mp.end(); it != mp.begin(); it--) { v.push_back(it->first); }
-    _map<int, int, std::greater<int> > mp1;
-    fillMap(mp1);
-    v.push_back(mp1.begin()->first);
-    mp1.erase(41);
-    v.push_back(mp1.begin()->first);
-    mp1.erase(29);
-    v.push_back(mp1.begin()->first);
-    _map<int, int, std::greater<int> > mp2;
-    mp2.insert(_make_pair(3, 3));
-    v.push_back(mp2.begin()->first);
-    mp2.erase(3);
-    if (mp2.begin() == mp2.end())
-        v.push_back(1);
-    _map<int, int, std::plus<int> > mp3;
-    fillMap(mp3);
-    for (typename _map<T, V>::iterator it = mp3.begin(); it != mp3.end(); it++) { v.push_back(it->first); }
-    for (typename _map<T, V>::iterator it = --mp3.end(); it != mp3.begin(); it--) { v.push_back(it->first); }
-    _map<int, int, std::minus<int> > mp4;
-    fillMap(mp4);
-    for (typename _map<T, V>::iterator it = mp4.begin(); it != mp4.end(); it++) { v.push_back(it->first); }
-    for (typename _map<T, V>::iterator it = --mp4.end(); it != mp4.begin(); it--) { v.push_back(it->first); }
-    _map<int, int, std::greater_equal<int> > mp5;
-    fillMap(mp5);
-    for (typename _map<T, V>::iterator it = mp5.begin(); it != mp5.end(); it++) { v.push_back(it->first); }
-    for (typename _map<T, V>::iterator it = --mp5.end(); it != mp5.begin(); it--) { v.push_back(it->first); }
-    _map<int, int, std::multiplies<int> > mp6;
-    fillMap(mp6);
-    for (typename _map<T, V>::iterator it = mp6.begin(); it != mp6.end(); it++) { v.push_back(it->first); }
-    for (typename _map<T, V>::iterator it = --mp6.end(); it != mp6.begin(); it--) { v.push_back(it->first); }
-    _map<int, int, std::bit_xor<int> > mp7;
-    fillMap(mp7);
-    for (typename _map<T, V>::iterator it = mp7.begin(); it != mp7.end(); it++) { v.push_back(it->first); }
-    for (typename _map<T, V>::iterator it = --mp7.end(); it != mp7.begin(); it--) { v.push_back(it->first); }
-    std::map<int, int, std::logical_and<int> > mp8;
-    fillMap(mp8);
-    for (typename std::map<T, V>::iterator it = mp8.begin(); it != mp8.end(); it++) { v.push_back(it->first); }
-    for (typename std::map<T, V>::iterator it = --mp8.end(); it != mp8.begin(); it--) { v.push_back(it->first); }
-    g_start1 = timer();
-    g_end1 = timer();
-    v.push_back(mp1.size());
-
-    return v;
-}
-
-int main() {
-
-    exit(run_map_unit_test<int, int>("compare class", comparator_test, comparator_test));
+	return (0);
 }
